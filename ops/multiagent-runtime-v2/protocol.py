fd352c0aa52587618f12e8b34a167160c6b8275c
Leandro Carlos <potiguaradm23@gmail.com>
2026-09-18 21:42:32 -0300

    feat(memory): register Cognitive Memory receipt schema

--- ops/multiagent-runtime-v2/protocol.py
+++ ops/multiagent-runtime-v2/protocol.py
@@ -1,3 +1,3 @@
 RELEASE='2.0.0'
-SCHEMAS={'mission_event':'mcf_mission_event/v2','recovery_bundle':'mcf_recovery_bundle/v1','live_history':'mcf_live_history/v1','runtime_metrics':'mcf_runtime_metrics/v1','runtime_policy':'mcf_runtime_policy/v1','adversarial_audit':'mcf_adversarial_audit/v1','capability_token':'mcf_capability_token/v1'}
+SCHEMAS={'mission_event':'mcf_mission_event/v2','recovery_bundle':'mcf_recovery_bundle/v1','live_history':'mcf_live_history/v1','runtime_metrics':'mcf_runtime_metrics/v1','runtime_policy':'mcf_runtime_policy/v1','adversarial_audit':'mcf_adversarial_audit/v1','capability_token':'mcf_capability_token/v1','cognitive_memory_receipt':'mcf_cognitive_memory_receipt/v1'}
 SUPPORTED_EXECUTION_BOUNDARY='CHATGPT_BUBBLE_LOCAL_SANDBOX'
