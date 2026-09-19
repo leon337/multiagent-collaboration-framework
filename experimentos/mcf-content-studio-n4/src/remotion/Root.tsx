bed3edc0d7b7e678c6a44033a235d86f9876c58b
Leandro Carlos <potiguaradm23@gmail.com>
2026-09-19 04:55:06 -0300

    test(content-studio): integrate visual QA and benchmark evidence

--- experimentos/mcf-content-studio-n4/src/remotion/Root.tsx
+++ experimentos/mcf-content-studio-n4/src/remotion/Root.tsx
@@ -1,6 +1,7 @@
 import {Composition} from 'remotion';
 import {RuntimeAgenticoPilot} from '../pilot/RuntimeAgenticoPilot';
 import {RUNTIME_AGENTICO_PILOT,RUNTIME_AGENTICO_PILOT_DURATION} from '../pilot/lesson';
+import {LongDiagramQa,LongFocusConceptQa,LongTimelineQa} from '../qa/VisualQaCompositions';
 import {
   BounceHeadlinePreviewLandscape,
   BounceHeadlinePreviewPortrait,
@@ -8,36 +9,19 @@ import {
 } from './PreviewCompositions';
 
 export const RemotionRoot=()=> <>
-  <Composition
-    id="RuntimeAgenticoPilot"
-    component={RuntimeAgenticoPilot}
-    durationInFrames={RUNTIME_AGENTICO_PILOT_DURATION}
-    fps={RUNTIME_AGENTICO_PILOT.fps}
-    width={RUNTIME_AGENTICO_PILOT.width}
-    height={RUNTIME_AGENTICO_PILOT.height}
-  />
-  <Composition
-    id="BounceHeadlinePreviewPortrait"
-    component={BounceHeadlinePreviewPortrait}
-    durationInFrames={120}
-    fps={30}
-    width={1080}
-    height={1920}
-  />
-  <Composition
-    id="BounceHeadlinePreviewLandscape"
-    component={BounceHeadlinePreviewLandscape}
-    durationInFrames={120}
-    fps={30}
-    width={1920}
-    height={1080}
-  />
-  <Composition
-    id="BounceHeadlinePreviewReducedMotion"
-    component={BounceHeadlinePreviewReducedMotion}
-    durationInFrames={120}
-    fps={30}
-    width={1080}
-    height={1920}
-  />
+  <Composition id="RuntimeAgenticoPilot" component={RuntimeAgenticoPilot}
+    durationInFrames={RUNTIME_AGENTICO_PILOT_DURATION} fps={RUNTIME_AGENTICO_PILOT.fps}
+    width={RUNTIME_AGENTICO_PILOT.width} height={RUNTIME_AGENTICO_PILOT.height}/>
+  <Composition id="BounceHeadlinePreviewPortrait" component={BounceHeadlinePreviewPortrait}
+    durationInFrames={120} fps={30} width={1080} height={1920}/>
+  <Composition id="BounceHeadlinePreviewLandscape" component={BounceHeadlinePreviewLandscape}
+    durationInFrames={120} fps={30} width={1920} height={1080}/>
+  <Composition id="BounceHeadlinePreviewReducedMotion" component={BounceHeadlinePreviewReducedMotion}
+    durationInFrames={120} fps={30} width={1080} height={1920}/>
+  <Composition id="LongFocusConceptQa" component={LongFocusConceptQa}
+    durationInFrames={180} fps={30} width={1080} height={1920}/>
+  <Composition id="LongTimelineQa" component={LongTimelineQa}
+    durationInFrames={180} fps={30} width={1080} height={1920}/>
+  <Composition id="LongDiagramQa" component={LongDiagramQa}
+    durationInFrames={180} fps={30} width={1080} height={1920}/>
 </>;
